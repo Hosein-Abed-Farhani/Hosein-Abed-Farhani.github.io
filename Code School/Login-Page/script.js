2473ad9ecb44ccf1953b3113c7b5dc583b2d5300
hosien <158040020+Hosein-Abed-Farhani@users.noreply.github.com>
2024-12-04 20:54:24 +0330

    Update script.js

---
+++ Code School/Login-Page/script.js
@@ -0,0 +1,9 @@
+function log_1(){
+    document.querySelector(".right").style.display = "none";
+    document.querySelector(".right-2").style.display = "flex";
+}
+
+function log_2(){
+    document.querySelector(".right-2").style.display = "none";
+    document.querySelector(".right").style.display = "flex";
+}
